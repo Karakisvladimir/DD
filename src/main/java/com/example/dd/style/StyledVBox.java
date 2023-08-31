bc37de7f7db3c496a9963a34ad09b06a01b7843d
Vladimir <karakisvladimir@gmail.com>
2023-08-31 19:51:56 +0300

    add Style class

---
+++ src/main/java/com/example/dd/style/StyledVBox.java
@@ -0,0 +1,18 @@
+package com.example.dd.style;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+public class StyledVBox {
+
+        public static VBox createStyledVBox(Label labelResult, Label labelTotalAccount, HBox hbox) {
+            VBox vBox = new VBox(labelResult,labelTotalAccount, hbox);
+            vBox.setAlignment(Pos.CENTER);
+            vBox.setSpacing(10);
+            vBox.setPadding(new Insets(10, 10, 10, 10));
+            return vBox;
+        }
+    }
+
