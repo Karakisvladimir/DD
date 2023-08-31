4a158e70bd0037f678698eaf1e534087a49c8521
Oleksii Samoliuk <samoluk.aleksey@gmail.com>
2023-08-31 19:14:54 +0300

    Added missed file to commit

--- src/main/java/com/example/dd/OnMoveButtonEventHandler.java
+++ src/main/java/com/example/dd/OnMoveButtonEventHandler.java
@@ -14,21 +14,19 @@ public class OnMoveButtonEventHandler implements EventHandler {
     private CityGetter cityGetter;
 
     public OnMoveButtonEventHandler(){
-
     }
-    public OnMoveButtonEventHandler(TextField userTextField,Label answerLabel, Label hintLabel) {//TODO:remove later this constructor
+    public OnMoveButtonEventHandler(TextField userTextField,Label answerLabel, Label hintLabel) {
         this.userTextField = userTextField;
         this.answerLabel = answerLabel;
         this.cityGetter = new CityGetterByFile();
         this.hintLabel = hintLabel;
-        answerLabel.setText(cityGetter.getLastCityByComputer());
-
+        this.answerLabel.setText(cityGetter.getLastCityByComputer());
     }
     @Override
     public void handle(Event event) {
         String cityByUser = userTextField.getText();
         if(cityGetter.validateCity(cityByUser)) {
-            if(!cityGetter.isCityUsed(cityByUser)){
+            if(!cityGetter.isCityUsed(cityByUser)) {
                 cityGetter.addProcessedCity(userTextField.getText());
                 String cityByComputer = cityGetter.getCity(cityByUser.charAt(cityByUser.length()-1));
                 if(cityByComputer.equals("")) {
