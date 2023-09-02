b6facb34beef0fa9e73cf62cd80ad57e1ee49074
Vladimir <karakisvladimir@gmail.com>
2023-09-02 23:24:37 +0300

    last version

--- src/main/java/com/example/dd/app/impl/OnMoveButtonEventHandler.java
+++ src/main/java/com/example/dd/app/impl/OnMoveButtonEventHandler.java
@@ -14,9 +14,10 @@ public class OnMoveButtonEventHandler implements EventHandler {
     private CityGetter cityGetter;
     private GameScore gameScore;
 
-    public OnMoveButtonEventHandler(){
+    public OnMoveButtonEventHandler() {
     }
-    public OnMoveButtonEventHandler(TextField userTextField,Label answerLabel, Label hintLabel) {
+
+    public OnMoveButtonEventHandler(TextField userTextField, Label answerLabel, Label hintLabel, GameScore gameScore) {
         this.userTextField = userTextField;
         this.answerLabel = answerLabel;
         this.cityGetter = new CityGetterByFile();
@@ -24,31 +25,33 @@ public class OnMoveButtonEventHandler implements EventHandler {
         this.answerLabel.setText(cityGetter.getLastCityByComputer());
         this.gameScore = new GameScore();
     }
+
     @Override
     public void handle(Event event) {
         gameScore.incrementHumanScore();
         String cityByUser = userTextField.getText();
-        if(cityGetter.validateCity(cityByUser)) {
-            if(!cityGetter.isCityUsed(cityByUser)) {
+        if (cityGetter.validateCity(cityByUser)) {
+            if (!cityGetter.isCityUsed(cityByUser)) {
                 cityGetter.addProcessedCity(userTextField.getText());
-                String cityByComputer = cityGetter.getCity(cityByUser.charAt(cityByUser.length()-1));
-                if(cityByComputer.equals("")) {
+                String cityByComputer = cityGetter.getCity(cityByUser.charAt(cityByUser.length() - 1));
+                if (cityByComputer.equals("")) {
                     TotalAccountWindow.show(gameScore);
-                }
-                else {
+                } else {
                     answerLabel.setText(cityByComputer);
                     hintLabel.setText("");
                     gameScore.incrementComputerScore();
                     userTextField.setText("");
                 }
-            }
-            else {
+            } else {
                 hintLabel.setText("City already used.");
             }
 
-        }
-        else {
+        } else {
             hintLabel.setText("Incorrect city name");
         }
+    }
+
+    public GameScore getGameScore() {
+        return gameScore;
     }
 }
