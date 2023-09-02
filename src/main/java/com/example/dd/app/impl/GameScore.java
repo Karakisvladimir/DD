ae588c08ee48ea6208b721248595976e8b883efa
Vladimir <karakisvladimir@gmail.com>
2023-09-02 14:36:13 +0300

    dont work score

---
+++ src/main/java/com/example/dd/app/impl/GameScore.java
@@ -0,0 +1,27 @@
+package com.example.dd.app.impl;
+
+public class GameScore {
+    private int humanScore = 0;
+    private int computerScore = 0;
+
+    public void incrementHumanScore(){
+        humanScore++;
+    }
+
+    public void incrementComputerScore(){
+        computerScore++;
+    }
+
+    public int getHumanScore(){
+        return humanScore;
+    }
+
+    public int getComputerScore(){
+        return computerScore;
+    }
+
+    public void resetScore(){
+        humanScore = 0;
+        computerScore = 0;
+    }
+}
