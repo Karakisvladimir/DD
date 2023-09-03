434c38b9647bafdcc854230da1a54570a35d34c4
Vladimir <karakisvladimir@gmail.com>
2023-09-03 21:07:14 +0300

    That All

--- src/main/java/com/example/dd/window/TotalAccountWindow.java
+++ src/main/java/com/example/dd/window/TotalAccountWindow.java
@@ -12,7 +12,6 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-//import static CityGetter.CityGetter.resetAll;
 
 public class TotalAccountWindow {
 
@@ -21,33 +20,36 @@ public class TotalAccountWindow {
         totalWindow.setTitle("TotalAccountWindow");
         totalWindow.setWidth(800);
         totalWindow.setHeight(400);
-        Label labelWin = new Label();
-        if (gameScore.getHumanScore() == gameScore.getComputerScore()) {
+        StyledLabel winLabelStyle = new StyledLabel(Color.GREEN);
+        Label labelWin = winLabelStyle.createStyledLabel(" ");
+        if (gameScore.getHumanScore() != gameScore.getComputerScore()) {
             labelWin.setText(" YOU ARE WIN, COMPUTER LOSE");
         } else {
             labelWin.setText("YOU ARE LOSE, COMPUTER WIN");
         }
-            // Добавила расчет счета
-            Label labelTotalWin = new Label("Total Account: Player score: " + gameScore.getHumanScore() + " / Computer score: " + gameScore.getComputerScore());
+        StyledLabel labelTotalWinLabelStyle = new StyledLabel(Color.DARKBLUE);
+        Label labelTotalWin = labelTotalWinLabelStyle.createStyledLabel("Total Account: Player score: " + gameScore.getHumanScore() + " / Computer score: " + gameScore.getComputerScore());
 
-            Button submitButton = new Button("PLAY AGAIN?");
-            submitButton.setOnAction(e -> {
-                totalWindow.close();
-                //Добавила вызов метода сброса счета
-                gameScore.resetScore();
 
-                MainWindow.show();
-            });
-            HBox hbox = new HBox(submitButton);
-            hbox.setAlignment(Pos.CENTER);
-            hbox.setSpacing(10);
-            VBox vBox = new VBox(labelWin, labelTotalWin, hbox);
-            vBox.setAlignment(Pos.CENTER);
-            vBox.setSpacing(10);
-            vBox.setPadding(new Insets(10, 10, 10, 10));
-            Scene mainScene = new Scene(vBox);
-            totalWindow.setScene(mainScene);
-            totalWindow.show(); // Здесь вы должны передать аргумент.
+        Button submitButton = new Button("PLAY AGAIN?");
+        submitButton.setOnAction(e -> {
+            totalWindow.close();
+
+
+            gameScore.resetScore();
+
+            MainWindow.show();
+        });
+        HBox hbox = new HBox(submitButton);
+        hbox.setAlignment(Pos.CENTER);
+        hbox.setSpacing(10);
+        VBox vBox = new VBox(labelWin, labelTotalWin, hbox);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setSpacing(10);
+        vBox.setPadding(new Insets(10, 10, 10, 10));
+        Scene mainScene = new Scene(vBox);
+        totalWindow.setScene(mainScene);
+        totalWindow.show();
 
     }
 
